76d7f957aa7403832cb68ad17cbded033b36fa72
Miilo1 <44490824+Miilo1@users.noreply.github.com>
2018-12-21 13:55:10 +0100

    Add files via upload

---
+++ Siege wald teil 2/Ground.java
@@ -0,0 +1,23 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Ground here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ground extends Actor
+{
+    public Ground () {
+        //getImage().scale(getImage().getWidth()*43, getImage().getHeight());
+        
+    }
+    /**
+     * Act - do whatever the Ground wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
