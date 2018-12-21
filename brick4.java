ddd19f1f5ae33825cbd4684c1f9bd7dcf43e4da0
SDF-FHMN <44286863+SDF-FHMN@users.noreply.github.com>
2018-12-21 12:49:51 +0100

    21.12

---
+++ brick4.java
@@ -0,0 +1,22 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class brick4 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class brick4 extends Ground
+{
+    public brick4(){
+        getImage().scale(getImage().getWidth()*2, getImage().getHeight());
+    }
+    /**
+     * Act - do whatever the brick4 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
